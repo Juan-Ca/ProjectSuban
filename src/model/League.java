0d1c6632642c4b4f6c4e5762551526f1cc41cc71
raquel6 <raquel.charron@gmail.com>
2015-10-29 10:00:28 -0400

    added domain model  # Please enter the commit message for your changes. Lines starting

---
+++ src/model/League.java
@@ -0,0 +1,321 @@
+package model;
+
+/*PLEASE DO NOT EDIT THIS CODE*/
+/*This code was generated using the UMPLE 1.22.0.5146 modeling language!*/
+
+
+import java.util.*;
+
+// line 1 "model.ump"
+// line 55 "model.ump"
+public class League
+{
+
+  //------------------------
+  // STATIC VARIABLES
+  //------------------------
+
+  private static League theInstance = null;
+
+  //------------------------
+  // MEMBER VARIABLES
+  //------------------------
+
+  //League Associations
+  private List<Player> players;
+  private List<Team> teams;
+  private List<Game> games;
+
+  //------------------------
+  // CONSTRUCTOR
+  //------------------------
+
+  private League()
+  {
+    players = new ArrayList<Player>();
+    teams = new ArrayList<Team>();
+    games = new ArrayList<Game>();
+  }
+
+  public static League getInstance()
+  {
+    if(theInstance == null)
+    {
+      theInstance = new League();
+    }
+    return theInstance;
+  }
+
+  //------------------------
+  // INTERFACE
+  //------------------------
+
+  public Player getPlayer(int index)
+  {
+    Player aPlayer = players.get(index);
+    return aPlayer;
+  }
+
+  public List<Player> getPlayers()
+  {
+    List<Player> newPlayers = Collections.unmodifiableList(players);
+    return newPlayers;
+  }
+
+  public int numberOfPlayers()
+  {
+    int number = players.size();
+    return number;
+  }
+
+  public boolean hasPlayers()
+  {
+    boolean has = players.size() > 0;
+    return has;
+  }
+
+  public int indexOfPlayer(Player aPlayer)
+  {
+    int index = players.indexOf(aPlayer);
+    return index;
+  }
+
+  public Team getTeam(int index)
+  {
+    Team aTeam = teams.get(index);
+    return aTeam;
+  }
+
+  public List<Team> getTeams()
+  {
+    List<Team> newTeams = Collections.unmodifiableList(teams);
+    return newTeams;
+  }
+
+  public int numberOfTeams()
+  {
+    int number = teams.size();
+    return number;
+  }
+
+  public boolean hasTeams()
+  {
+    boolean has = teams.size() > 0;
+    return has;
+  }
+
+  public int indexOfTeam(Team aTeam)
+  {
+    int index = teams.indexOf(aTeam);
+    return index;
+  }
+
+  public Game getGame(int index)
+  {
+    Game aGame = games.get(index);
+    return aGame;
+  }
+
+  public List<Game> getGames()
+  {
+    List<Game> newGames = Collections.unmodifiableList(games);
+    return newGames;
+  }
+
+  public int numberOfGames()
+  {
+    int number = games.size();
+    return number;
+  }
+
+  public boolean hasGames()
+  {
+    boolean has = games.size() > 0;
+    return has;
+  }
+
+  public int indexOfGame(Game aGame)
+  {
+    int index = games.indexOf(aGame);
+    return index;
+  }
+
+  public static int minimumNumberOfPlayers()
+  {
+    return 0;
+  }
+
+  public boolean addPlayer(Player aPlayer)
+  {
+    boolean wasAdded = false;
+    if (players.contains(aPlayer)) { return false; }
+    players.add(aPlayer);
+    wasAdded = true;
+    return wasAdded;
+  }
+
+  public boolean removePlayer(Player aPlayer)
+  {
+    boolean wasRemoved = false;
+    if (players.contains(aPlayer))
+    {
+      players.remove(aPlayer);
+      wasRemoved = true;
+    }
+    return wasRemoved;
+  }
+
+  public boolean addPlayerAt(Player aPlayer, int index)
+  {  
+    boolean wasAdded = false;
+    if(addPlayer(aPlayer))
+    {
+      if(index < 0 ) { index = 0; }
+      if(index > numberOfPlayers()) { index = numberOfPlayers() - 1; }
+      players.remove(aPlayer);
+      players.add(index, aPlayer);
+      wasAdded = true;
+    }
+    return wasAdded;
+  }
+
+  public boolean addOrMovePlayerAt(Player aPlayer, int index)
+  {
+    boolean wasAdded = false;
+    if(players.contains(aPlayer))
+    {
+      if(index < 0 ) { index = 0; }
+      if(index > numberOfPlayers()) { index = numberOfPlayers() - 1; }
+      players.remove(aPlayer);
+      players.add(index, aPlayer);
+      wasAdded = true;
+    } 
+    else 
+    {
+      wasAdded = addPlayerAt(aPlayer, index);
+    }
+    return wasAdded;
+  }
+
+  public static int minimumNumberOfTeams()
+  {
+    return 0;
+  }
+
+  public boolean addTeam(Team aTeam)
+  {
+    boolean wasAdded = false;
+    if (teams.contains(aTeam)) { return false; }
+    teams.add(aTeam);
+    wasAdded = true;
+    return wasAdded;
+  }
+
+  public boolean removeTeam(Team aTeam)
+  {
+    boolean wasRemoved = false;
+    if (teams.contains(aTeam))
+    {
+      teams.remove(aTeam);
+      wasRemoved = true;
+    }
+    return wasRemoved;
+  }
+
+  public boolean addTeamAt(Team aTeam, int index)
+  {  
+    boolean wasAdded = false;
+    if(addTeam(aTeam))
+    {
+      if(index < 0 ) { index = 0; }
+      if(index > numberOfTeams()) { index = numberOfTeams() - 1; }
+      teams.remove(aTeam);
+      teams.add(index, aTeam);
+      wasAdded = true;
+    }
+    return wasAdded;
+  }
+
+  public boolean addOrMoveTeamAt(Team aTeam, int index)
+  {
+    boolean wasAdded = false;
+    if(teams.contains(aTeam))
+    {
+      if(index < 0 ) { index = 0; }
+      if(index > numberOfTeams()) { index = numberOfTeams() - 1; }
+      teams.remove(aTeam);
+      teams.add(index, aTeam);
+      wasAdded = true;
+    } 
+    else 
+    {
+      wasAdded = addTeamAt(aTeam, index);
+    }
+    return wasAdded;
+  }
+
+  public static int minimumNumberOfGames()
+  {
+    return 0;
+  }
+
+  public boolean addGame(Game aGame)
+  {
+    boolean wasAdded = false;
+    if (games.contains(aGame)) { return false; }
+    games.add(aGame);
+    wasAdded = true;
+    return wasAdded;
+  }
+
+  public boolean removeGame(Game aGame)
+  {
+    boolean wasRemoved = false;
+    if (games.contains(aGame))
+    {
+      games.remove(aGame);
+      wasRemoved = true;
+    }
+    return wasRemoved;
+  }
+
+  public boolean addGameAt(Game aGame, int index)
+  {  
+    boolean wasAdded = false;
+    if(addGame(aGame))
+    {
+      if(index < 0 ) { index = 0; }
+      if(index > numberOfGames()) { index = numberOfGames() - 1; }
+      games.remove(aGame);
+      games.add(index, aGame);
+      wasAdded = true;
+    }
+    return wasAdded;
+  }
+
+  public boolean addOrMoveGameAt(Game aGame, int index)
+  {
+    boolean wasAdded = false;
+    if(games.contains(aGame))
+    {
+      if(index < 0 ) { index = 0; }
+      if(index > numberOfGames()) { index = numberOfGames() - 1; }
+      games.remove(aGame);
+      games.add(index, aGame);
+      wasAdded = true;
+    } 
+    else 
+    {
+      wasAdded = addGameAt(aGame, index);
+    }
+    return wasAdded;
+  }
+
+  public void delete()
+  {
+    players.clear();
+    teams.clear();
+    games.clear();
+  }
+
+}
---
+++ src/model/ShotOnGoal.java
@@ -0,0 +1,83 @@
+package model;
+
+/*PLEASE DO NOT EDIT THIS CODE*/
+/*This code was generated using the UMPLE 1.22.0.5146 modeling language!*/
+
+
+
+// line 30 "model.ump"
+// line 79 "model.ump"
+public class ShotOnGoal
+{
+
+  //------------------------
+  // MEMBER VARIABLES
+  //------------------------
+
+  //ShotOnGoal Attributes
+  private boolean goal;
+
+  //ShotOnGoal Associations
+  private Player suban;
+
+  //------------------------
+  // CONSTRUCTOR
+  //------------------------
+
+  public ShotOnGoal(boolean aGoal, Player aSuban)
+  {
+    goal = aGoal;
+    if (!setSuban(aSuban))
+    {
+      throw new RuntimeException("Unable to create ShotOnGoal due to aSuban");
+    }
+  }
+
+  //------------------------
+  // INTERFACE
+  //------------------------
+
+  public boolean setGoal(boolean aGoal)
+  {
+    boolean wasSet = false;
+    goal = aGoal;
+    wasSet = true;
+    return wasSet;
+  }
+
+  public boolean getGoal()
+  {
+    return goal;
+  }
+
+  public Player getSuban()
+  {
+    return suban;
+  }
+
+  public boolean setSuban(Player aNewSuban)
+  {
+    boolean wasSet = false;
+    if (aNewSuban != null)
+    {
+      suban = aNewSuban;
+      wasSet = true;
+    }
+    return wasSet;
+  }
+
+  public void delete()
+  {
+    suban = null;
+  }
+
+
+  public String toString()
+  {
+	  String outputString = "";
+    return super.toString() + "["+
+            "goal" + ":" + getGoal()+ "]" + System.getProperties().getProperty("line.separator") +
+            "  " + "suban = "+(getSuban()!=null?Integer.toHexString(System.identityHashCode(getSuban())):"null")
+     + outputString;
+  }
+}
